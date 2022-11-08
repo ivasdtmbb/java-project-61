5f600fc1708959b7f089ee517e869aa5bcd2231c
ivasdtmbb <ivasdtmbb@gmail.com>
2022-11-08 16:53:10 +0600

    Add Makefile and Cli class
    The Makefile run an executable 'app', the Cli.java starts a dialog with user

---
+++ Makefile
@@ -0,0 +1,5 @@
+# Makefile
+
+# Run the executable 'app' file
+run-dist:
+	./app/build/install/app/bin/app
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,10 @@
 // hexlet/code/App.java
-
 package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Cli dialog = new Cli();
+        dialog.chat();
     }
 }
