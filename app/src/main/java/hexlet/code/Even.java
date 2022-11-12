39b79429b1ff2cdbcfd76426b726e33dfc21e716
ivasdtmbb <ivasdtmbb@gmail.com>
2022-11-12 21:37:30 +0600

    Add Even.java & StartMenu.java files

---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class BrainGames {
+
+    public static void even() {
+        Cli.chat();
+
+        Scanner scanner = new Scanner(System.in);
+
+        Stystem.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+        
+    }
+}
---
+++ app/src/main/java/hexlet/code/StartMenu.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public class Menu {
+
+    public static void maint(String args) {
+        System.out.print("")
+    }
+}
