c71a3d83847527c4a4928e6bf7b258b6c02fe359
ivasdtmbb <ivasdtmbb@gmail.com>
2022-11-13 14:03:34 +0600

    Add class Calc.java
    Implement a Calculator program

---
+++ app/src/main/java/hexlet/code/Calc.java
@@ -0,0 +1,53 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Calc {
+
+    public static void start(int attempts) {
+        var userName = Cli.start();
+        var scanner = new Scanner(System.in);
+        var randomRange = 100;
+        int number1;
+        int number2;
+        int correctAnswer;
+        char[] operators = {'*', '-', '+'};
+        char operator;
+
+        System.out.println("What is the result of the expression?");
+
+        for (var i = 0; i < attempts; i++) {
+            operator = operators[RandomGenerator.generateInt(operators.length - 1)];
+            number1 = RandomGenerator.generateInt(randomRange);
+            number2 = RandomGenerator.generateInt(randomRange);
+            correctAnswer = getResult(number1, number2, operator);
+
+            System.out.println("Question: " + number1 + " " + operator + " " + number2);
+            System.out.print("Your answer: ");
+            int userAnswer = Integer.parseInt(scanner.next());
+
+            if (userAnswer == correctAnswer) {
+                System.out.println("Correct!");
+            } else {
+                UserDialog.wrongAnswer(String.valueOf(userAnswer),
+                        String.valueOf(correctAnswer), userName);
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + userName + "!");
+    }
+
+    public static int getResult(int number1, int number2, char operator) {
+        switch (operator) {
+            case '*':
+                return number1 * number2;
+            case '-':
+                return number1 - number2;
+            case '+':
+                return number1 + number2;
+            default:
+                break;
+        }
+        return 0;
+    }
+}
