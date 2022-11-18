531389e15d40e38cdb32193d4034d1a587f4dffe
ivasdtmbb <ivasdtmbb@gmail.com>
2022-11-18 09:55:47 +0600

    Change a class UserDialog.java

--- app/src/main/java/hexlet/code/UserDialog.java
+++ app/src/main/java/hexlet/code/UserDialog.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UserDialog {
     public static String start() {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.print("1 - Greet\n"
                 + "2 - Even\n"
@@ -16,7 +16,11 @@ public class UserDialog {
                 + "0 - Exit\n");
 
         System.out.print("Your choice: ");
-        return scanner.nextLine();
+        var choice = userInput.nextLine();
+        System.out.println();
+
+        return choice;
+
     }
 
     public static void wrongAnswer(String userAnswer, String correctAnswer, String userName) {
@@ -28,5 +32,29 @@ public class UserDialog {
                 userName
         );
         System.out.println(message);
+    }
+
+    public static void winner(String userName) {
+        System.out.println("Congratulations, " + userName + "!");
+    }
+
+    public static boolean gameDialog(String question, String correctAnswer, String userName) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Question: " + question);
+        System.out.print("Your answer: ");
+        String userAnswer = userInput.next();
+
+        userInput.close();
+
+        if (userAnswer.equals(correctAnswer)) {
+            return true;
+        } else {
+            wrongAnswer(userAnswer, correctAnswer, userName);
+            return false;
+        }
+    }
+
+    public static void correct() {
+        System.out.println("Correct!");
     }
 }
