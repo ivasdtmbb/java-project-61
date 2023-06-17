3f166d8082ae363ac4e77de468eae98f8c758fd4
ivasdtmbb <ivasdtmbb@gmail.com>
2023-06-17 10:12:42 +0300

    Modify App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,8 +5,22 @@ public class App {
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        var userAnswer = Cli.start(userInput);
+        var numberOfAttempts = 3;
+        var userAnswer = UserDialog.start(userInput);
 
-        userInput.close();
+        switch (userAnswer) {
+            case "1":
+                Cli.start(userInput);
+                userInput.close();
+                break;
+            case "2":
+                Even.start(userInput, numberOfAttempts);
+                userInput.close();
+                break;
+            default:
+                break;
+        }
+
+
     }
 }
