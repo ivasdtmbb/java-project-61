ec7e467b5da557ddc8f99ac842999929e38d4589
ivasdtmbb <ivasdtmbb@gmail.com>
2023-06-19 07:33:50 +0300

    Create Engine class

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,48 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static String menu(Scanner userInput) {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.print("1 - Greet\n"
+                + "2 - Even\n"
+                + "3 - Calc\n"
+                + "4 - GCD\n"
+                + "5 - Progression\n"
+                + "6 - Prime\n"
+                + "0 - Exit\n");
+
+        System.out.print("Your choice: ");
+        String choise = userInput.next();
+        System.out.println();
+
+        return choise;
+    }
+
+    public static String getUserAnswer(String question, Scanner userInput) {
+        System.out.println("Question: " + question);
+        System.out.print("Your answer: ");
+        var userAnswer = userInput.next();
+
+        return userAnswer;
+    }
+    public static void printCorrect() {
+        System.out.println("Correct!");
+    }
+
+    public static void printWinner(String userName) {
+        System.out.println("Congratulations, " + userName + "!");
+    }
+
+    public static void printWrongAnswer(String userAnswer, String correctAnswer, String userName) {
+        String message = String.format(
+                "'%s' is wrong answer ;(. Correct answer was '%s'.\n"
+                        + "Let's try again, %s!",
+                        userAnswer,
+                        correctAnswer,
+                        userName
+                        );
+        System.out.println(message);
+    }
+}
